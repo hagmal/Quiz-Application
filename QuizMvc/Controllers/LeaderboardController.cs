74c0aca67f366324c7a97649176d84f503903823
hagmal <mahag2193@oslomet.no>
2025-09-10 15:00:13 +0200

    Lagt til Take og Leaderboard controllers og sider

---
+++ QuizMvc/Controllers/LeaderboardController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class LeaderboardController : Controller
+    {
+        // GET: LeaderboardController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ QuizMvc/Controllers/TakeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class TakeController : Controller
+    {
+        // GET: TakeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
