ec739709acbbaa43fbb4836934e293538c93aafe
hagmal <mahag2193@oslomet.no>
2025-09-10 14:44:33 +0200

    Laget midlertidig navbar + midlertidig side for quizzes

---
+++ QuizMvc/Controllers/QuizzesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class QuizzesController : Controller
+    {
+        // GET: QuizzesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
